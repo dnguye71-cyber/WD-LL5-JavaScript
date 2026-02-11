5437075cf4408054c55279901d427b68bbfc29a2
Dylan Nguyen <dnguye71@asu.edu>
2026-02-11 21:07:56 +0000

    customize alerts and adding a console log.

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 console.error("This is my first JavaScript error!");
-alert("Dylan Team 4"); 
-alert("Paulina"); 
-alert("Becky");
-alert("Mica");
-alert("Veroni");
+console.log("This is Team 4 and Here are the Team members!");
+alert("Dylan, Paulina, Becky, Mica, and Veroni!"); 
+alert("Welcome to my Site! Thank you for Visiting!");
